c3872138ede2677dad7a2308c93878b477c11f0b
Ayden Letourneau <ar.letourneau@baypath.net>
2023-01-17 15:42:20 +0000

    added backend tables and asociations

---
+++ app/models/section.rb
@@ -0,0 +1,5 @@
+class Section < ApplicationRecord
+    has_many :pages
+    validates :title, :location, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
+  resources :sponsors
+  resources :photos
+  resources :albums
+  resources :events
+  resources :pages
+  get 'home/home'
+  resources :sections
+  
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "pages#home"
+   root "home#home"
 end
