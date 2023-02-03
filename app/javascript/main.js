141949f8987f5881ada8336161aaa3df512a1c68
Ayden Letourneau <ar.letourneau@baypath.net>
2023-02-03 19:19:15 +0000

    2/3/2023

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,16 +1,52 @@
-let height = screen.height;
-let container = document.getElementById('pagecon');
-let pageHeight = container.offsetHeight;
-
-console.log(height);
-console.log(pageHeight);
+var height = screen.height;
+var width = screen.width;
+var container = document.getElementById('pagecon');
+var pageHeight = container.offsetHeight;
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
 
 if (pageHeight < height) {
-    container.classList.add('vh-100');
-}
+        container.classList.add('vh-100');
+    }
 
-if (pageHeight > height) {
-    if (container.classList.contains('vh-100')) {
+    if (w < 750) {
         container.classList.remove('vh-100');
     }
-}
+
+    if (pageHeight > height) {
+    if (container.classList.contains('vh-100')) {
+        container.classList.remove('vh-100');
+        }
+    }     
+
+
+window.addEventListener('resize', function() {
+
+    console.log("In Event Listener");
+
+    var height = screen.height;
+    var width = screen.width;
+    var container = document.getElementById('pagecon');
+    var pageHeight = container.offsetHeight;
+
+    var w = document.documentElement.clientWidth;
+    var h = document.documentElement.clientHeight;
+
+    console.log(h);
+    console.log(w);
+
+
+    if (pageHeight < height) {
+        container.classList.add('vh-100');
+    }
+
+    if (w < 750) {
+        container.classList.remove('vh-100');
+    }
+
+    if (pageHeight > height) {
+    if (container.classList.contains('vh-100')) {
+        container.classList.remove('vh-100');
+        }
+    }     
+});
